56f8e155b438c33a4387e3b3b9f655debd04c932
Havindu Dissanayake <havindu1992@gmail.com>
2022-12-03 08:09:22 +1100

    Seperated the Monster Load and implemented a clear function to test out the react declarative nature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import React, {Component} from 'react';
+import React,{Component} from 'react';
+import button from 'react-dom';
 
 class App extends Component {
   constructor(){
     super();
 
+    //  Application State is Stored Here.
     this.state = {
       monsters : [],
       searchFeild: ''
@@ -12,14 +14,31 @@ class App extends Component {
 
   }
 
+  // When this component is mounted, all the monsters are loaded and set to the state. 
   componentDidMount(){
+    this.loadMonsters();
+  }
+
+  loadMonsters = () => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters: users}))
     .catch(error => console.log(error));
   }
+  
+  clearMonsters = () => {
+    this.setState({monsters: []});
+  }
+
 
   render(){
+
+    let monsterLoadButton;
+
+    if(this.state.monsters.length === 0){
+      monsterLoadButton = <button onClick={()=>this.loadMonsters()}>Load Monsters</button>
+    }
+
     return(
       <div className='App'>
         <h1>Monsters</h1>
@@ -28,10 +47,11 @@ class App extends Component {
             return (<p key={monster.id} >{monster.name + monster.phone}</p>)
           })
         }
+        <button onClick={()=>this.clearMonsters()}>Clear Monsters</button><br/><br/>
+        {monsterLoadButton}
       </div>
     );
   }
-
 }
 
 export default App;
