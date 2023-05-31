50dc535a57c261cd805f641c5844c43b9f33fab8
Havindu Piyumal Dissanayake <havindu1992@gmail.com>
2023-05-31 20:41:38 +1000

    project set up and testing react concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Havindu",
+    };
+  }
+
+  onButtonClick = (event) => {
+    this.setState({ name: "Havindu Dissanayake" });
+  };
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Hello World {name}</h1>
+        <button onClick={this.onButtonClick}>View Full Name</button>
+      </div>
+    );
+  }
 }
 
 export default App;
